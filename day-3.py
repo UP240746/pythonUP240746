99ac05450eb6005696e94143142954f573a8d45f
Janize <up240746@alumnos.upa.edu.mx>
2025-02-26 09:30:45 -0600

    Dia 3

---
+++ day-3.py
@@ -0,0 +1,43 @@
+#EJERCICIOS DIA 3 
+# Declara tu edad como variable entera 
+
+edad= 18 
+print("Tu edad es: ",edad)
+
+#Declara tu estatura como una varibale del tipo float
+
+estatura= 1.60 # es una variable flotante por que tiene decimal 
+print("Tu estatura es de:", estatura) 
+
+#Area de un triangulo 
+
+print("Area de un triangulo")
+base= int(input("Introduce la base del triangulo:"))
+altura= int(input("Introduce la altura del triangulo"))
+
+area= ( 0.5 * base * altura )
+
+print("El resultado es:", area )
+
+#Perimetro de un triangulo
+print("Perimetro de un triangulo")
+lado_a= int(input("introduzca el lado a del triangulo: "))
+lado_b=int(input("introduzca el lado b del triangulo: "))
+lado_c=int(input("introduzca el lado c del triangulo:"))
+
+perimetro= (lado_a + lado_b + lado_c) 
+
+print("el resultado es:", perimetro)
+
+#Longitud  y Anchura de un rectangulo 
+
+
+
+
+
+
+
+
+
+
+
