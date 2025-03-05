28c33e8993442232374e9533692caf27752e8c39
Janize <up240746@alumnos.upa.edu.mx>
2025-03-05 10:06:39 -0600

    dia4

--- day_4.py
+++ day_4.py
@@ -1,1 +1,36 @@
 #Ejercicios dia 4 :)
+
+#ejercicio 1 
+#Concatenar la cadena de "30 dias de pytlhon" en una sola cadena 
+print("Ejercicio numero 1")
+n1= "Treinta"
+n2= "dias"
+n3= "de" 
+n4= "python" 
+print("Cadena:", n1 + " " + n2 +" " + n3 +" " + n4 )
+
+#ejercicio 2
+# concatenar frase 
+print("Ejercicio numero 2")
+n1= "Codificacion"
+n2= "para"
+n3= "todos"
+print("cadena2:", n1 +" " + n2 +" " + n3 )
+
+#ejercicio 3
+#Declara una variable llamada empresa y asignale un valor inicial "Codificar para todos"
+print("Ejercicio 3")
+empresa= "Codificar para todos"
+print("variable: ", empresa)
+
+#ejercicio 4
+print("Ejercicio 4") 
+#ejercicio 5
+#ejercicio 6
+#ejercicio 7
+#ejercicio 8 
+#ejercicio 9 
+#ejercicio 10 
+#ejercicio 11
+#ejercicio 12
+#
