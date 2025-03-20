eca7ce434adaeef3ff29a4c57ae6940f65c21176
Janize <up240746@alumnos.upa.edu.mx>
2025-03-20 11:47:12 -0600

    holiwis

---
+++ 07_Day/day_7p2.py
@@ -0,0 +1,17 @@
+#Ejercicios dia 7 parte 2 
+#sets
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+print("Set 1:", A)
+print("Set 2:", B)
+
+#Ejercicio1 p2
+#Une A y B 
+print("Ejercicio 1 p2")
+union= A.union(B)
+print(union)
+
+#Ejercicio2 p2
+#Buscar interseccion A y B 
+print("Ejercicio 2 p2")
+
