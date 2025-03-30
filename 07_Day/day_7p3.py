790225a50056bef2c63866b5844a4ab2d35c77f5
Janize <up240746@alumnos.upa.edu.mx>
2025-03-30 12:45:29 -0600

    sisisi

--- 07_Day/day_7p3.py
+++ 07_Day/day_7p3.py
@@ -17,17 +17,21 @@ else:
 #Ejercicio 2 
 #Explique la diferencia entre los siguientes tipos de datos: cadena, lista, tupla y set
 print("Ejercicio 2 p3 ")
+print("La funcion del string es: ")
 print ("La cadena se encierra solo entre comillas y no se puede modificar después de ser creada")
+print("La funcion de la lista es: ")
 print("La lista es un conjunto de elementos entre corchetes, se pueden cambiar, agregar y eliminar elementos")
+print("La fincion de la tupla es: ")
 print("La tupla es similar a la lista, se encierra entre paréntesis y no se pueden modificar los elementos")
+print("La funcion del set es: ")
 print("El set es un conjunto de elementos que no se repiten pero sí se pueden modificar, se encierra entre llaves")
 
 #Ejercicio 3 p3
 #Cuantas palabras nuevas usadas en la oracion, utiliza los metodos spl
 print("Ejercicio 3 p3")
-fraces = "I am a teacher and I love to inspire and teach people"
-print(frases)
-words = fraces.split()
-uniqueWords = set(words)
-print("Palabras únicas:" , len(uniqueWords) , uniqueWords)
-                                                                                                                   
+fraces = "Soy un profesor, me gusta inspirar y enseñar personas"
+print(fraces)
+palabras= fraces.split()
+palabrasUnicas = set(palabras)
+print("Palabras únicas:" , len(palabrasUnicas), palabrasUnicas)
+                                                                    
