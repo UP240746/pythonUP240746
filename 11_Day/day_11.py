b1cf6b8a58474f736cecf7bd636dc83df66555f5
Janize <up240746@alumnos.upa.edu.mx>
2025-04-03 11:49:47 -0600

    toycansao

--- 11_Day/day_11.py
+++ 11_Day/day_11.py
@@ -33,4 +33,102 @@ def añadirTodoslosNum(*args):
 print(añadirTodoslosNum(1,2,3,4,5,6))
 print(añadirTodoslosNum("string", 1,2,3))
 
+#Ejercicio 4 p1
+#La termperatura en grados C puede ser convertida a grados F. 
+#Ecribe una funcion para convertir grados C a F 
+print("Ejercicio 4 p1 ")
+def celcius_a_fahrenheit(celcius):
+    fahrenheit= (celcius * 9 / 5 ) + 32
+    return fahrenheit
+print("La temperatura en Fahrenheit es:", celcius_a_fahrenheit(50))
+
+#Ejercicio 5 p1 
+#Escribe una funcion llamada comprobar_estacion, toma un parametro de mes y asignale la temporada 
+print("Ejercicio 5 p1 ")
+def comprobar_estacion (mes):
+    if mes in ["Enero", "Febrero", "Marzo"]:
+        return "invierno"
+    elif mes in ["Abril","Mayo", "Junio"]:
+        return "Primavera"
+    elif mes in ["Julio","Agosto", "Septiembre"]:
+        return "Verano"
+    else: 
+        return "Otoño"
+print("La estacion de Enero es: ", comprobar_estacion(mes= "Enero"))
+
+
+#Ejercicio 6 p1 
+#Escribe una funcion llamada calcular_pendiente que devuelva la pendiente a una ecuacion lineal 
+print("Ejercicio 6 p1")
+print("x1=8")
+print("x2= 6")
+print("y1= 4")
+print("y2= 2")
+def calcular_pendiente(x1,x2,y1,y2):
+    pen=(y2-y1) / (x2-x1)
+    return pen 
+print("La pendiente de la ecuacion es: ", calcular_pendiente(8,6,4,2))
+
+#Ejercicio 7 p1 
+#La ecuación cuadrática se calcula del siguiente modo: ax² + bx + c = 0. 
+# Escriba una función que calcule el conjunto solución de una ecuación cuadrática
+print("Ejercicio 7 p1 ")
+def res_ec_cuadratica(A,B,C):
+    D= B ** 2 - 4 * A * C
+    if D > 0:
+        x1 = (- B + D ** 0.5) / (2 * A)
+        x2 = (- B - D ** 0.5) / (2 * A)
+        return x1, x2 
+    elif D==0: 
+        x= -B/(2* A)
+        return x
+    else: 
+        return "No hay solucion"
+print("El resultado de la ecuacion cuadratica es: ", "x=", res_ec_cuadratica(1,4,8))
+
+#Ejercicio 8 p1
+#Declara una funcion llamada imprime_lista 
+#Toma una lista como parametro e imprime cada elemeto de la lista 
+print("Ejercicio 8 p1")
+def imprime_lista(lista):
+    for item in lista: 
+        print(item)
+marcasCarros=["Nissan", "Toyota", "Jeep", "BMW"]
+print("Maracas de carros")
+print(marcasCarros)
+
+#Ejercico 9 p1 
+#Declara una funcion llamada invertir_lista
+#Toma un array como parametro y devuelve el inverso del array (usa bucles)
+print("Ejercicio 9 p1")
+print("Lista:", marcasCarros)
+def invertir_lista(array):
+    invertir_lista=[]
+    for item in array:
+        return invertir_lista
+print("Invertir lista: ", invertir_lista(marcasCarros))
+
+#Ejercicio 10 p1 
+#Toma una lista como parametro y devuelve una lista de elementos en mayusculas 
+print("Listas: ", marcasCarros)
+def mayusculasLista(Lista):
+    return [item.upper()for item in Lista]
+print("Lista en mayusculas: ", mayusculasLista(marcasCarros))
+
+#Ejercicio 11 p1 
+#Declatra una funcion llamada añade_item.
+#Toma como parametro una lista y un lemento, devuelve la lista con el añadido final
+print("Ejeficio 11 p1 ")
+def añade_item (lista, item): 
+    lista.appened(item)
+    return lista 
+print("La lista es: ",añade_item(marcasCarros, "Mercedes Benz"))
+
+#Ejercicio 12 p1
+#Declara una funcion llamada remove_item 
+#Toma una lista y un elemento 
+
+
+
+
     
